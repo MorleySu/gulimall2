4822a1c6a1f278d0816a1455e7d3c0c8d355a4ab
MorleySu <951666991@qq.com>
2022-12-08 14:07:09 +0800

    P27学完，并且按照视频中的版本进行依赖管理

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -1,17 +1,19 @@
-package com.atguigu.gulimall.coupon;
+package com.atguigu.gulimall.member;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@MapperScan("com.atguigu.gulimall.coupon.dao")
+@MapperScan("com.atguigu.gulimall.member.dao")
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @SpringBootApplication
-public class GulimallCouponApplication {
+public class GulimallMemberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GulimallCouponApplication.class, args);
+		SpringApplication.run(GulimallMemberApplication.class, args);
 	}
 
 }
